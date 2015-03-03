23537abcd0589918573e48cdc6d7853081779b81
Michael Heasell <michael.heasell@gmail.com>
2015-03-03 12:42:22 +0000

    Extract server address to config file

--- hearts.js
+++ hearts.js
@@ -1,17 +1,15 @@
-require(['jquery', 'knockout', 'services/QueueService', 'mainModel', 'bootstrap'],
-    function($, ko, QueueService, MainModel) {
+require(['jquery', 'knockout', 'services/QueueService', 'mainModel', 'config', 'bootstrap'],
+    function($, ko, QueueService, MainModel, config) {
 
     $(function() {
         "use strict";
-
-        var serverAddress = "http://192.168.1.12:5000";
 
         ko.components.register('queueView', { require: 'components/queueView/queueView' });
         ko.components.register('gameView', { require: 'components/gameView/gameView' });
 
         var mainModel = new MainModel();
 
-        var svc = new QueueService(serverAddress);
+        var svc = new QueueService(config.serverAddress);
         mainModel.setComponent('queueView', { manager: mainModel, service: svc });
 
         ko.applyBindings(mainModel);
